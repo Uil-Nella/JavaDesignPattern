84418af3c738b2c2224caaa71182cb712334849b
刘新宇 <liuxinyu03@meituan.com>
2015-03-26 17:57:39 +0800

    添加cglib动态代理

---
+++ src/main/java/org/bugkillers/pattern/proxy/CglibProxy.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package org.bugkillers.pattern.proxy;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+
+
+
+/**
+ * 使用cglib动态代理
+ *
+ * Created by liuxinyu on 15/3/26.
+ *
+ */
+public class CglibProxy implements MethodInterceptor {
+    private Object target;
+
+    /**
+     * 创建代理对象
+     *
+     * @param target
+     * @return
+     */
+    public Object getInstance(Object target) {
+        this.target = target;
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(this.target.getClass());
+        // 回调方法
+        enhancer.setCallback(this);
+        // 创建代理对象
+        return enhancer.create();
+    }
+
+    @Override
+    // 回调方法
+    public Object intercept(Object obj, Method method, Object[] args,
+                            MethodProxy proxy) throws Throwable {
+        Object result =null;
+        before();
+        result = proxy.invokeSuper(obj, args);
+        after();
+        return result;
+
+
+    }
+
+    public void before(){
+        System.out.println("before method！");
+    }
+
+
+
+    public void after(){
+        System.out.println("after method！");
+    }
+
+
+}
