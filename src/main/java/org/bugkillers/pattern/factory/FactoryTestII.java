ce69003c5816c9d2b311beed0a77fff40e0e2c4b
刘新宇 <liuxinyu03@meituan.com>
2015-04-03 10:06:26 +0800

    添加多方法工厂类

---
+++ src/main/java/org/bugkillers/pattern/factory/FactoryTestII.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package org.bugkillers.pattern.factory;
+
+/**
+ * Created by liuxinyu on 15/4/3.
+ */
+public class FactoryTestII {
+    public static void main(String[] args) {
+        ComputerFactoryII.produceMac().work();
+        ComputerFactoryII.produceMac().work();
+        ComputerFactoryII.produceMsi().work();
+    }
+}
