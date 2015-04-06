5ba95e49342d1baaf0454bbf7306ddf42de5e77e
刘新宇 <liuxinyu03@meituan.com>
2015-04-06 17:57:56 +0800

    添加对象的适配器

---
+++ src/main/java/org/bugkillers/pattern/adapter/SourceSub1.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2015. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package org.bugkillers.pattern.adapter;
+
+public class SourceSub1 extends AbstractWrapper {
+	public void method1(){
+		System.out.println("the sourceable interface's first Sub1!");
+	}
+}
