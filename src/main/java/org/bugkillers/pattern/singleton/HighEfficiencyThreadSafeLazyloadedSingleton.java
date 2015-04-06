595ee37800cb0b4951779252faed31be0ef85545
刘新宇 <liuxinyu03@meituan.com>
2015-04-06 14:06:37 +0800

    添加高效的单例模式

--- src/main/java/org/bugkillers/pattern/singleton/HighEfficiencyThreadSafeLazyloadedSingleton.java
+++ src/main/java/org/bugkillers/pattern/singleton/HighEfficiencyThreadSafeLazyloadedSingleton.java
@@ -9,6 +9,8 @@
 package org.bugkillers.pattern.singleton;
 
 /**
+ * 一个相对完善的单例模式，避免了synchronized关键字对方法加锁，同时对方法加锁时采取了对class的monitor加锁
+ * 相对于对方法加锁，有一定程度的提高，在单例私有构造器的多次创建也采取的抛出异常的处理。
  * Created by liuxinyu on 15/4/6.
  */
 public class HighEfficiencyThreadSafeLazyloadedSingleton {
@@ -17,10 +19,29 @@ public class HighEfficiencyThreadSafeLazyloadedSingleton {
      */
     private static HighEfficiencyThreadSafeLazyloadedSingleton instance;
 
+    /**
+     * 私有化构造器，并在多线程中不被多次创建，并在防止使用反射的工具创建实例时抛出异常。
+     */
     private HighEfficiencyThreadSafeLazyloadedSingleton() {
         if (instance != null) {
             throw new IllegalStateException("实例已经被初始化");
         }
+    }
+
+    /**
+     * 单例获取方法
+     * 创建单例时需要对实例进行校验，只有实例没有创建时才对其进行加锁创建实例
+     *
+     * @return
+     */
+    public static HighEfficiencyThreadSafeLazyloadedSingleton getInstance() {
+        if (instance != null) {
+            synchronized (HighEfficiencyThreadSafeLazyloadedSingleton.class) {
+                instance = new HighEfficiencyThreadSafeLazyloadedSingleton();
+            }
+        }
+
+        return instance;
     }
 
 }
